365bed7e385eecabf5b0deb3b265b23828315ecc
Azamat <azamatshamshiev18@gmail.com>
2023-05-16 16:15:55 +0300

    aza

---
+++ src/Pages/mainMenu/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const mainMenu = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default mainMenu;
---
+++ src/Pages/moderInterior/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const moderInterior = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default moderInterior;
