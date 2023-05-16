c850c06eb2d0312690dca2df31e3884928a77c75
Azamat <azamatshamshiev18@gmail.com>
2023-05-17 02:34:26 +0300

    aza

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from "./components/Header";
+import ItalianCuisine from "./Pages/Hero";
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <ItalianCuisine/>
     </div>
   );
 }
---
+++ src/Pages/Hero/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import heroWave from "../../images/hero-wave.svg"
+import heroWaveRight from "../../images/hero-wave-right.svg"
+
+const Hero = () => {
+    return (
+        <div id="cuisine">
+            <div className="container">
+                <div className="cuisine">
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
