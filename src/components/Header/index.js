e5dffc41375f64f7f5232bf7c0fae25b34fb81dc
Azamat <azamatshamshiev18@gmail.com>
2023-05-16 15:44:21 +0300

    sdf

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+       <header>
+           <div className="container">
+               <div className="header">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, ipsa!
+               </div>
+           </div>
+       </header>
+    );
+};
+
+export default Header;
