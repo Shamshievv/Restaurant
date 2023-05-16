c22a6da758b1b323559cdad9c9c299838ee9cc1c
Azamat <azamatshamshiev18@gmail.com>
2023-05-16 16:30:23 +0300

    aza

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
+import {FiSearch} from "react-icons/fi";
 
 const Header = () => {
     return (
        <header>
            <div className="container">
                <div className="header">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, ipsa!
+                  <p>Restaurant</p>
+                   <div className="header--nav">
+                       <NavLink to={`/`}>
+                           interior
+                       </NavLink>
+                       <NavLink to={`/`}>
+                           About Us
+                       </NavLink>
+                       <NavLink to={`/`}>
+                           Menu
+                       </NavLink>
+                       <NavLink to={`/`}>
+                           Contacts
+                       </NavLink>
+                       <button>
+                           <FiSearch/>
+                       </button>
+                   </div>
+
                </div>
            </div>
        </header>
