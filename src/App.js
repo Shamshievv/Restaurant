88c31cde5f9bb32fccf442853afd82b210ca98ae
Azamat <azamatshamshiev18@gmail.com>
2023-05-19 00:19:59 +0300

    aza

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Header from "./components/Header";
 import ItalianCuisine from "./Pages/Hero";
+import Visit from "./Pages/Visit";
 
 function App() {
   return (
     <div className="App">
     <Header/>
     <ItalianCuisine/>
+     <Visit/>
     </div>
   );
 }
