84f177f548fe3429b8b030268d2ca90d4a6d1243
nurgazievbekbolsun <nurgazievbekbolsun906@gmail.com>
2023-05-20 19:03:15 +0600

    az

--- src/Pages/Visit/index.js
+++ src/Pages/Visit/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import * as diagnostics_channel from "diagnostics_channel";
 
 const Visit = () => {
     return (
         <div>
 
         </div>
+
     );
 };
+
 
 export default Visit;
