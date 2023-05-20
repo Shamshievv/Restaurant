23ab4dc8aeb810729897ea9df3c8bf794d04fb08
nurgazievbekbolsun <nurgazievbekbolsun906@gmail.com>
2023-05-20 19:20:51 +0600

    aza

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.scss';
 import Header from "./components/Header";
 
+import Visit from "./Pages/Visit";
+import Footer from "./Pages/Footer";
+import {Route, Routes} from "react-router-dom";
+import Hero from "./Pages/Hero";
+import About from "./Pages/About";
+import MainMenu from "./Pages/mainMenu";
+
 function App() {
-  return (
-    <div className="App">
-    <Header/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Routes>
+                <Route path={"/about"} element={<About/>}/>
+                <Route path={"/menu"} element={<MainMenu/>}/>
+            </Routes>
+            <Hero/>
+            <Visit/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
+
