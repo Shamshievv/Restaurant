62ddd17ee68a6943672db52673b391e9e1e480e9
nurgazievbekbolsun <nurgazievbekbolsun906@gmail.com>
2023-05-20 16:09:47 +0600

    add bord

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.scss';
 import Header from "./components/Header";
-import About from "./Pages/About";
-import BestSellers from "./Pages/BestSellers";
 
 function App() {
   return (
-      <div className="App">
-        <Header/>
-        <About/>
-        <BestSellers />
-      </div>
+    <div className="App">
+    <Header/>
+    </div>
   );
 }
 
